0f6b445a386df445a8f5d1f32c7f80bd70605c2b
Titus Wormer <tituswormer@gmail.com>
2022-05-27 09:49:37 +0200

    Move core to `lib/`

--- test.js
+++ test.js
@@ -2,7 +2,9 @@
  * @typedef {import('estree-jsx').Program} Program
  * @typedef {import('estree-jsx').Comment} Comment
  * @typedef {import('estree-jsx').Node} Node
- * @typedef {import('./index.js').Node} HastNode
+ * @typedef {import('hast').Root} Root
+ * @typedef {import('hast').Content} Content
+ * @typedef {Root|Content} HastNode
  */
 
 import test from 'tape'
